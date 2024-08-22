646815f0e79ca55840d9c16b46fc66098804d43b
devcoyo <tituskiplagat06@gmail.com>
2024-08-22 15:40:26 +0300

    feat: Solution to task 1 - Printing Hello world to screen

---
+++ 4-tasked-php-exercise/task1-solution.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task 1</title>
+</head>
+
+<body>
+    <h1>
+        This is a PHP script that displays hello world using php
+    </h1>
+    <?php
+    echo "Hello world";
+    ?>
+
+</body>
+
+</html>
