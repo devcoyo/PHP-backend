9c58996d303614af5ace2176149368619d626119
devCoyo <tituskiplagat06@gmail.com>
2024-08-22 15:14:33 +0300

    feat: variables

---
+++ 1-introduction/variable.php
@@ -0,0 +1,11 @@
+<?php
+ function local_scope(){
+    $num = 45;
+    echo "local varibale is decalred as $num <br>";
+
+    // getting sum of two numbers
+    $num_2 = 20;
+    $sum_of_nums = $num + $num_2;
+    echo "sum of two numbers is $sum_of_nums";
+ }
+local_scope();
