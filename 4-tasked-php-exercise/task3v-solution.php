41de99e9449686c9f174e9b5d2657d93ebdb12a5
devcoyo <tituskiplagat06@gmail.com>
2024-08-23 08:29:59 +0300

    a variable age that prints if person is minor or major

---
+++ 4-tasked-php-exercise/task3v-solution.php
@@ -0,0 +1,8 @@
+<?php
+    //Define a variable age and print whether the person is an adult or a minor based on the value.
+    $age = $user_1_age;
+    if ($age >= 18) {
+        echo "Adult";
+    } else {
+        echo "Minor";
+    }
