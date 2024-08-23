a86dd610d92f17d9a8a377c88dd48ab46e5ed7f0
devcoyo <tituskiplagat06@gmail.com>
2024-08-23 08:26:48 +0300

    feat: task two solution converting string to int, float using (int)$var_name

---
+++ 4-tasked-php-exercise/task2v-solution.php
@@ -0,0 +1,9 @@
+<?php
+    echo "<h1>Converting a string to an integer and a float</h1>";
+    $string_num = "45";
+    $integer_num = (int)$string_num;
+    $float_num = (float)$string_num;
+    echo "String number: $string_num <br>";
+    echo "Integer number: $integer_num <br>";
+    echo "Float number: $float_num <br>";   
+?>
